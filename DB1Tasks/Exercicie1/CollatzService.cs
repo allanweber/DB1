39a41598f4f8d4c7601d9426ab7bcb70347212de
allan.weber <allan.weber@cwi.com.br>
2018-01-19 15:07:11 -0200

    Excercício 1 tarefas de 1 a 3

---
+++ DB1Tasks/Exercicie1/CollatzService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicie1
+{
+    public class CollatzService
+    {
+        private int sequenceLength = 0;
+        public int SequenceLength { get { return sequenceLength; } }
+
+        private int startingNumber;
+        public int StartingNumber { get { return startingNumber; } }
+
+        private int maxNumber;
+
+        public CollatzService(int maxNumber)
+        {
+            this.maxNumber = maxNumber;
+        }
+
+        public void Calc()
+        {
+            long sequence;
+
+            int[] cache = new int[this.maxNumber + 1];
+
+            for (int i = 0; i < cache.Length; i++)
+            {
+                cache[i] = -1;
+            }
+            cache[1] = 1;
+
+            for (int index = 2; index <= this.maxNumber; index++)
+            {
+                sequence = index;
+                int k = 0;
+                while (sequence != 1 && sequence >= index)
+                {
+                    k++;
+                    if (this.NumberIsEven(sequence))
+                        sequence = this.CalcEven(sequence);
+                    else
+                        sequence = this.CalcOdd(sequence);
+
+                }
+                cache[index] = k + cache[sequence];
+
+                if (cache[index] > sequenceLength)
+                {
+                    this.sequenceLength = cache[index];
+                    this.startingNumber = index;
+                }
+            }
+        }
+
+        private bool NumberIsEven(long number)
+        {
+            return number % 2 == 0;
+        }
+
+        private long CalcEven(long number)
+        {
+            return number / 2;
+        }
+
+        private long CalcOdd(long number)
+        {
+            return (3 * number) + 1;
+        }
+    }
+}
---
+++ DB1Tasks/Exercicie1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1 - Tarefa 1
+            CollatzProblem();
+
+            // Exercício 1 - Tarefa 2
+            JustOddNmbers();
+
+            // Exercício 1 - Tarefa 3
+            DifferenceBetweenCollections();
+
+            Console.ReadKey();
+        }
+
+        private static void CollatzProblem()
+        {
+            Console.WriteLine("Exercício 1, tarefa 1");
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int oneMillion = 1000000;
+            CollatzService service = new CollatzService(oneMillion);
+            service.Calc();
+
+            Console.WriteLine($"O número {service.StartingNumber} produz a maior sequencia de {service.SequenceLength} iterações.");
+
+            watch.Stop();
+            Console.WriteLine($"Essa solução levou {watch.Elapsed.ToString()} para executar.");
+            Console.WriteLine();
+        }
+
+        private static void JustOddNmbers()
+        {
+            Console.WriteLine("Exercício 1, tarefa 2");
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int[] numbers = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+
+            Func<int, bool> oddNumberWhere = (int number) => { return number % 2 != 0; };
+            var result = numbers.All(oddNumberWhere);
+
+            Console.WriteLine($"A lista{(!result ? " não" : string.Empty)} contém somente números ímpares");
+
+            watch.Stop();
+            Console.WriteLine($"Essa solução levou {watch.Elapsed.ToString()} para executar.");
+            Console.WriteLine();
+        }
+
+        private static void DifferenceBetweenCollections()
+        {
+            Console.WriteLine("Exercício 1, tarefa 3");
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            int[] firstArray = { 1, 3, 7, 29, 42, 98, 234, 93 };
+
+            int[] secondArray = { 4, 6, 93, 7, 55, 32, 3 };
+
+            List<int> result = firstArray.Where(num => !secondArray.Contains(num)).ToList();
+
+            Console.WriteLine($"Lista de diferença entre arrays: { String.Join(",", result.ConvertAll(i => i.ToString()).ToArray()) }");
+
+            watch.Stop();
+            Console.WriteLine($"Essa solução levou {watch.Elapsed.ToString()} para executar.");
+            Console.WriteLine();
+        }
+    }
+}
